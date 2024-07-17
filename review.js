c5d10361bb1090c26a39671428b17ddb0ed2e214
Samuel Etling <samueletling@gmail.com>
2024-07-17 14:52:28 -0600

    added some JS to the cart

--- review.js
+++ review.js
@@ -5,3 +5,9 @@ const popoverList = [...popoverTriggerList].map(
   (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
 );
 console.log("hello world");
+
+const cardArr = [...document.querySelectorAll(".product-card")].map(
+  (item) => item.innerHTML
+);
+
+console.log(cardArr);
